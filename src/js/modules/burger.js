08280b7c26ad5c5ea2faa89d71f896580ed7dfd1
Ivan <ionel.cainareanu1@gmail.com>
2020-07-19 10:21:24 +0200

    navigation burger fixed, about me block photo added

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -1,12 +1,29 @@
 let burger = () => {
     let navToggle = document.querySelector('#navToggle'),
-        nav = document.querySelector('#nav')
+        nav = document.querySelector('#nav'),
+        backgroundDiv = document.querySelector('#background'),
+        navLinks = document.querySelectorAll('.nav__link')
+
 
     navToggle.addEventListener('click', (e) => {
-        event.preventDefault()
+        e.preventDefault()
 
         nav.classList.add('show')
+        backgroundDiv.style.display = 'flex'
+
     })
+
+    backgroundDiv.addEventListener('click', () => {
+        nav.classList.remove('show')
+        backgroundDiv.style.display = 'none'
+    })
+
+    navLinks.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('show')
+            backgroundDiv.style.display = 'none'
+        })
+    });
 
 }
 
